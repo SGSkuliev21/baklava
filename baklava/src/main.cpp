4f2550f80b2ec833bbefb6bb4baa173a3560ddde
IYStoychev21 <IYStoychev21@codingburgas.bg>
2022-11-30 21:43:48 +0200

    Make the tower disapper when health is 0. Close #18

--- baklava/src/main.cpp
+++ baklava/src/main.cpp
@@ -23,7 +23,7 @@ int main()
     );
     
     // Initializing the tower stats: attack speed, attack power, defense, health, regen, size and position
-    TowerStats towerStats = { 1.0f, 2.0f, 3.0f, 150.0f, 5.0f, Vector3({3.0f, 9.0f, 3.0f}), Vector3({0.0f, 4.5f, 0.0f}) };
+    TowerStats towerStats = { 1.0f, 2.0f, 3.0f, 3.0f, 5.0f, Vector3({3.0f, 9.0f, 3.0f}), Vector3({0.0f, 4.5f, 0.0f}) };
  
     // Initializing enemy system
     const int enemyLimit = 6, debounceTimer = 0.4*fpsCap;
@@ -98,7 +98,11 @@ int main()
 
             }
  
-        drawTower(towerStats, BLUE, DARKBLUE);
+            if (towerStats.towerHealth > 0)
+            {
+                drawTower(towerStats, BLUE, DARKBLUE);
+            
+            }
                    
         EndMode3D();
 
